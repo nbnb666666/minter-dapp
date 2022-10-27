f0fde69400d761b392c5e4c6381644c67dfc9e57
Kevin Z <467922647@qq.com>
2022-10-27 12:43:58 +1100

    d

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -287,7 +287,7 @@ function setTotalPrice() {
   }
   console.log(info.deploymentConfig)
   console.log(mintInputValue)
-  const totalPriceWei = BigInt(info.deploymentConfig.mint_Price) * BigInt(mintInputValue);
+  const totalPriceWei = BigInt(MINT_PRICE) * BigInt(mintInputValue);
   
   let priceType = '';
   if(chain === 'goerli' || chain === 'ethereum') {
